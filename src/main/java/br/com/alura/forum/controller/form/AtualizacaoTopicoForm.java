e11fa5885c398b427616f4d22001511fc238b5ea
Almir <almirjr.87@gmail.com>
2019-12-05 14:01:19 -0300

    Conclusão da parte 1

---
+++ src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class AtualizacaoTopicoForm {
+
+    @NotBlank
+    @Size(min = 5)
+    private String titulo;
+
+    @NotBlank
+    @Size(min = 10)
+    private String mensagem;
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+        Topico topico = topicoRepository.getOne(id);
+        topico.setTitulo(this.titulo);
+        topico.setMensagem(this.mensagem);
+
+        return topico;
+    }
+}
